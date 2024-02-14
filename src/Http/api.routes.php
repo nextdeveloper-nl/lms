6d237c8bdc77926079d3ffcad1cd17bbf9e28a6b
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-02-14 13:40:12 +0300

    Module generated again

--- src/Http/api.routes.php
+++ src/Http/api.routes.php
@@ -1,6 +1,26 @@
 <?php
 
-Route::prefix('lms')->group(function () {
-// EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE
+Route::prefix('lms')->group(
+    function () {
+        // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE
+        Route::prefix('courses')->group(
+            function () {
+                Route::get('/', 'Courses\CoursesController@index');
 
-});
+                Route::get('{lms_courses}/tags ', 'Courses\CoursesController@tags');
+                Route::post('{lms_courses}/tags ', 'Courses\CoursesController@saveTags');
+                Route::get('{lms_courses}/addresses ', 'Courses\CoursesController@addresses');
+                Route::post('{lms_courses}/addresses ', 'Courses\CoursesController@saveAddresses');
+
+                Route::get('/{lms_courses}/{subObjects}', 'Courses\CoursesController@relatedObjects');
+                Route::get('/{lms_courses}', 'Courses\CoursesController@show');
+
+                Route::post('/', 'Courses\CoursesController@store');
+                Route::patch('/{lms_courses}', 'Courses\CoursesController@update');
+                Route::delete('/{lms_courses}', 'Courses\CoursesController@destroy');
+            }
+        );
+    }
+);
+
+
